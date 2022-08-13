8eb30decb2c105e850a5ea71ab8616ded88cc95d
Md Sumon Islam <sumon.99.sis@gmail.com>
2022-08-13 13:10:06 +0600

    new calender added

--- test.js
+++ test.js
@@ -1,12 +1,80 @@
 
-var a = 4;
-var b = 5;
-var c = add(4,9);
+function valu_chk()
+{
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
 
-function add(a,b){
-    return a+b;
+    if( isNaN(num1) || isNaN(num2) || num1 == '' || num2 =='')
+    {
+        alert('Enter valid number!')
+        return;
+    }
+}
+function displayalt()
+{
+    var conf = confirm("Do you want to clear")
+    if (conf == true) 
+    {
+         document.getElementById("add").innerHTML = "";
+         document.getElementById('num1').value = "";
+         document.getElementById('num2').value = "";
+    }
 }
 
+function sum()
+{
+    valu_chk();
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("add").innerHTML = num3;
+}
 
-console.log("Result: ", c);
+function min()
+{
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = parseFloat(num1) - parseFloat(num2);
+    document.getElementById("add").innerHTML = num3;
+}
 
+function sub()
+{
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = parseFloat(num1) * parseFloat(num2);
+    document.getElementById("add").innerHTML = num3;
+}
+
+function div()
+{
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = parseFloat(num1) / parseFloat(num2);
+    document.getElementById("add").innerHTML = num3;
+}
+
+function callLoop(){
+    var eseries = "";
+    var oseries = "";
+    var ll = prompt("enter lower limit!");
+    var ul = prompt("enter upper limit!");
+
+    for(let i=ll; i<=ul; i++)
+    {
+        if(i%2 == 0){
+            {
+                eseries = eseries + i + "<br>";
+            }
+        }
+        if(i%2 == 1){
+            {
+                oseries = oseries + i + "<br>";
+            }
+        }
+        
+        document.getElementById("s-loop").innerHTML = eseries;
+        
+        document.getElementById("o-loop").innerHTML = oseries;
+    }
+}
