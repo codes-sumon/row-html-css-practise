d90f5873b45503cde6066c8457d471c1f90166ba
Md Sumon Islam <sumon.99.sis@gmail.com>
2022-08-16 23:44:31 +0600

    Home page done

--- New folder/date.js
+++ New folder/date.js
@@ -40,8 +40,6 @@ function getDate() {
     var date = d.getDate();
 
     h2.innerHTML = day + ' '+date + ', ' + month +' '+ d.getFullYear();
-    //h3.innerHTML = 'Time: '+d.getHours()+':'+d.getMinutes();
-
     return {
         m: month,
         dy: day,
@@ -49,3 +47,19 @@ function getDate() {
         yr: d.getFullYear()
     };
 }
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =  "Time: "+ h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
